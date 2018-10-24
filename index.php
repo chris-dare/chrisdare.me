a514e24a7ec40f414dd076c68bbb41e80cffbacf
Chris Dare <christopher.dare@dareafrica.io>
2018-10-24 15:09:26 +0000

    fix paragraphing

--- index.php
+++ index.php
@@ -28,7 +28,15 @@
 						<div class="col-sm-6">
 							<h1 class="mb0 uppercase bold italic">CHRIS DARE</h1>
 							<h5 class="uppercase mb32">sOFTWARE DEVELOPER | TECHNOLOGY ANALYST | CEO IN TRAINING</h5>
-							<p class="lead mb0">CSU '14 | SEO Africa '17 | VVU '17 | DECAM LC | CMU Hopeful |&nbsp;&nbsp;<br><br>Experienced in Technology Consulting, Business Analysis, Software Development and Product Innovation.<br><br>&nbsp; &nbsp; My mission is to drive change and innovation across my home: The African Continent. This place was once the center of global trade. Soon it will be again; <i>the world is resetting and Africa stands to ride over the storm that's brewing.</i> I'm choosing to stay to here and join our future heroes unfold the story - ready to work with my sleeves folded up.<br><br>I'm a visionary; my perceptive ability causes me to see possibilities everywhere. I get excited and enthusiastic about my ideas, and spread my enthusiasm to others. Being blessed with ideas everyday also means it's a little tough to stick to one and grind - but I'm gradually gaining mastery over that. I'm perfecting myself into becoming the ultimate innovator and&nbsp;learning to work with, understand and motivate people as well as create value to sell.<br><br>This one is for the geeks and nerds: <i>As a retired coding polyglot, I now focus on coding with Python and JS (React.js to be more specific). The AI dream is getting more real.</i><br></p>
+							<p class="lead">CSU '14 | SEO Africa '17 | VVU '17 | DECAM LC | CMU Hopeful</p>
+							
+							<p class="lead">Experienced in Technology Consulting, Business Analysis, Software Development and Product Innovation.</p>
+							
+							<p class="lead">My mission is to drive change and innovation across my home: The African Continent. This place was once the center of global trade. Soon it will be again; <i>the world is resetting and Africa stands to ride over the storm that's brewing.</i> I'm choosing to stay to here and join our future heroes unfold the story - ready to work with my sleeves folded up.</p>
+							
+							<p class="lead">I'm a visionary; my perceptive ability causes me to see possibilities everywhere. I get excited and enthusiastic about my ideas, and spread my enthusiasm to others. Being blessed with ideas everyday also means it's a little tough to stick to one and grind - but I'm gradually gaining mastery over that. I'm perfecting myself into becoming the ultimate innovator and&nbsp;learning to work with, understand and motivate people as well as create value to sell.</p>
+							
+							<p class="lead">This one is for the geeks and nerds: <i>As a retired coding polyglot, I now focus on coding with Python and JS (React.js to be more specific). The AI dream is getting more real.</i><br></p>
 						</div>
 					</div>
 					
@@ -41,7 +49,7 @@
 		        <div class="row mb64 mb-xs-24">
 		            <div class="col-sm-12 text-center">
 		                <h3>Experience</h3>
-		                <p class="lead">I've been taught to stand out by the quality and impact of my work. If it's not enough, I pause, consider and then I work smarter.<br>This is my work experience within the last two years</p>
+		                <p class="lead">I've been taught to stand out by the quality and impact of my work. If it's not enough I pause, consider and then I work smarter.<br>This is my work experience within the last two years</p>
 		            </div>
 		        </div>
 		        
@@ -62,7 +70,14 @@
 		                                            <h4>Graduated from Valley View University <br>with a CS Degree. An interest in investing<br>led me to SEO Africa<br></h4>
 		                                        </div>
 		                                    </div>
-		                                    <p>● CS Major at Valley View University<br>● President and Senator, Computing Science Students' Association (2016-2017)<br>● Graduated with a 3.31/4.00 CGPA and a 3.87 math CGPA<br>● Awarded "Best behaved Student"<br>●&nbsp;Admitted into the SEO Africa local internship program to work with Inflexion Capital Limited.<br>● Acquired skills during the training program in Accounting &amp; Financial Statement Analysis,&nbsp;Case Study Analysis &amp; Group Presentations,&nbsp;Advanced Microsoft Excel, PowerPoint &amp; Word, Networking,&nbsp;Financial News Review, Fine Dining Etiquette, Networking, Oral and Written Communication<br></p>
+		                                    <p>
+												<ol>● CS Major at Valley View University</ol>
+												<ol>● President and Senator, Computing Science Students' Association (2016-2017)</ol>
+												<ol>● Graduated with a 3.31/4.00 CGPA and a 3.87 math CGPA</ol>
+												<ol>● Awarded "Best behaved Student"</ol>
+												<ol>●&nbsp;Admitted into the SEO Africa local internship program to work with Inflexion Capital Limited.</ol>
+												<ol>● Acquired skills during the training program in Accounting &amp; Financial Statement Analysis,&nbsp;Case Study Analysis &amp; Group Presentations,&nbsp;Advanced Microsoft Excel, PowerPoint &amp; Word, Networking,&nbsp;Financial News Review, Fine Dining Etiquette, Networking, Oral and Written Communication</ol>
+											</p>
 		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
 		                                </div>
 		                                <div>
@@ -73,11 +88,15 @@
 		                                            <h4><i>Analyst,&nbsp;</i>IC Asset Managers (Ghana) Limited</h4>
 		                                        </div>
 		                                    </div>
-		                                    <p>IC Asset Managers, an Inflexion Capital subsidiary, is a leading fund manager, serving retail and institutional and sovereign clients with over USD 189mn  in assets under management.<br><br>● Worked in a team of 8 to increase Assets under Management by 13.5% from USD 167mn to USD 189mn in 6 months&nbsp;<br>● Worked with the Chief Operations Officer to optimized the Firm's business processes to guide the development of new software for the firm
-		<br>● Wrote test scripts to validate a cash fund software product built for the firm
-		<br>● Automated the generation of fund performance reports for all pension funds managed by the firm with MS Power BI<br>● Presented on behalf of the fund manager to pension fund clients and trustees
-		<br>● Prepared client meeting reports
-		<br></p>
+											<p>IC Asset Managers, an Inflexion Capital subsidiary, is a leading fund manager, serving retail and institutional and sovereign clients with over USD 189mn  in assets under management.</p>
+											<p>
+												<ol>● Worked in a team of 8 to increase Assets under Management by 13.5% from USD 167mn to USD 189mn in 6 months</ol>
+												<ol>● Worked with the Chief Operations Officer to optimized the Firm's business processes to guide the development of new software for the firm</ol>
+												<ol>● Wrote test scripts to validate a cash fund software product built for the firm</ol>
+												<ol>● Automated the generation of fund performance reports for all pension funds managed by the firm with MS Power BI</ol>
+												<ol>● Presented on behalf of the fund manager to pension fund clients and trustees</ol>
+												<ol>● Prepared client meeting reports</ol>
+											</p>
 		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
 		                                </div>
 		                            </div>
@@ -91,11 +110,16 @@
 		                                    <div class="overflow-hidden">
 		                                        <img alt="Pic" class="mb24 pull-left" src="img/logo - Inflexion Capital Limited.png">
 		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">January 2018 - APRIL 2018</h6>
+		                                            <h6 class="uppercase mb8 number">January 2018 - April 2018</h6>
 		                                            <h4>Analyst, Inflexion Capital Limited</h4>
 		                                        </div>
 		                                    </div>
-		                                    <p>Worked in the technology division: Inflexion Technologies, now a subsidiary: DARE Africa.<br><br>● Developed software requirements and specifications for an instant liquidity investment app<br>● Built a KPI analytics application to monitor the subsidiary performance with Python and PgSQL. Set up the application on Google Cloud (Compute Engine + Cloud Storage)<br>● Performed unit and usability tests for software products (cash fund and stock subscription) for IC Asset Managers and IC Securities Holdings Limited<br></p>
+		                                    <p>Worked in the technology division: Inflexion Technologies, now a subsidiary: DARE Africa.</p>
+											<p>	
+												<ol>● Developed software requirements and specifications for an instant liquidity investment app
+												<ol>● Built a KPI analytics application to monitor the subsidiary performance with Python and PgSQL. Set up the application on Google Cloud (Compute Engine + Cloud Storage)
+												<ol>● Performed unit and usability tests for software products (cash fund and stock subscription) for IC Asset Managers and IC Securities Holdings Limited<br>
+											</p>
 		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
 		                                </div>
 		                                <div>
@@ -106,15 +130,17 @@
 		                                            <h4>Analyst, <br>MTN Ghana - IC Securities (Ghana) Limited</h4>
 		                                        </div>
 		                                    </div>
-											<p>Worked with an Inflexion Subsidiary, IC Securities, to raise USD 787mn for Scancom PLC (MTN Ghana) - the biggest telco in Ghana and most visible brand in the country.
-											&nbsp;IC Securities is an Africa-focused investment banking and securities firm, serving sovereign, institutional, corporate and individual clients.<br>
-												<br>● Handled technical support for 30 Banks and Brokerage institutions as well as retail investors during the MTN Ghana share offer
-												<br>● Performed usability tests on software updates during the share offer
-												<br>● Worked on technical and policy documentation for IC Securities Holdings Limited
-												<br>● Copywriting for IC Securities TradeLive platform<br>● Performed usability tests on IC Securities TradeLive platform
-												<br>● Ran financial reconciliations for all USSD subscriptions
-												<br>● Devised IC Securities' retail business strategy in a team with management<br>
-												<br>● Building a wealth management subsidiary with Inflexion Capital Management<br>
+											<p>Worked with an Inflexion Subsidiary, IC Securities, to raise USD 787mn for Scancom PLC (MTN Ghana) - the biggest telco in Ghana and most visible brand in the country.</p>
+											&nbsp;IC Securities is an Africa-focused investment banking and securities firm, serving sovereign, institutional, corporate and individual clients.</ol>
+											<p>
+												<ol>● Handled technical support for 30 Banks and Brokerage institutions as well as retail investors during the MTN Ghana share offer</ol>
+												<ol>● Performed usability tests on software updates during the share offer</ol>
+												<ol>● Worked on technical and policy documentation for IC Securities Holdings Limited</ol>
+												<ol>● Copywriting for IC Securities TradeLive platform</ol>
+												<ol>● Performed usability tests on IC Securities TradeLive platform</ol>
+												<ol>● Ran financial reconciliations for all USSD subscriptions</ol>
+												<ol>● Devised IC Securities' retail business strategy in a team with management</ol>
+												<ol>● Building a wealth management subsidiary with Inflexion Capital Management</ol>
 											</p>
 		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
 		                                </div>
