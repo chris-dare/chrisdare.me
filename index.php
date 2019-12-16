03ecb8bfc7f3f1329759919c5da583b922659f21
Chris Dare <cdare@andrew.cmu.edu>
2019-12-16 18:32:43 +0200

    Added new placeholder for website upgrade

--- index.php
+++ index.php
@@ -1,221 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="hero is-info is-bold is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+            Chris Dare
+          </h1>
+          
+          <p class="subtitle">
+            Entrepreneurial leader and innovator with expertise at the intersection of product
+            management, software engineering and communication. Focusses on developing and
+            optimizing solutions for business growth. Past work increased customer bases, revenue
+            and profit margins for clients and partners. Seeking to harnessing applications of AI and
+            machine learning to design, build and manage analytics/cognitive solutions for
+            organizations.
+          </p>
 
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Chris Dare - Smart Creative | CEO in Training</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/theme-navy.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
-        <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
-        <link href="http://fonts.googleapis.com/css?family=Montserrat:100,300,400,600,700" rel="stylesheet" type="text/css">
-        <link href="css/font-montserrat.css" rel="stylesheet" type="text/css">
-    </head>
+          <p class="subtitle">
+              Currently majoring in applied machine learning at Carnegie Mellon University. 
+              <br><b>Also searching for a summer internship in research/engineering.</b> Areas of interest: Product management, machine learning, deep learning, speech recognition.
+          </p>
 
-    <body class="btn-rounded">
-				
-		<div class="nav-container">
-		</div>
-		
-		<div class="main-container">
-		<section class="image-bg overlay pt240 pb240 pt-xs-180 pb-xs-180">
-				<div class="background-image-holder">
-					<img alt="image" class="background-image" src="img/chris-dare-at-MTN.jpeg">
-				</div>
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-6">
-							<h1 class="mb0 uppercase bold italic">CHRIS DARE</h1>
-							<h5 class="uppercase mb32">sOFTWARE DEVELOPER | TECHNOLOGY ANALYST | CEO IN TRAINING</h5>
-							<p class="lead">CSU '14 | SEO Africa '17 | VVU '17 | DECAM LC | CMU Hopeful</p>
-							
-							<p class="lead">Experienced in Technology Consulting, Business Analysis, Software Development and Product Innovation.</p>
-							
-							<p class="lead">My mission is to drive change and innovation across my home: The African Continent. This place was once the center of global trade. Soon it will be again; <i>the world is resetting and Africa stands to ride over the storm that's brewing.</i> I'm choosing to stay to here and join our future heroes unfold the story - ready to work with my sleeves folded up.</p>
-							
-							<p class="lead">I'm a visionary; my perceptive ability causes me to see possibilities everywhere. I get excited and enthusiastic about my ideas, and spread my enthusiasm to others. Being blessed with ideas everyday also means it's a little tough to stick to one and grind - but I'm gradually gaining mastery over that. I'm perfecting myself into becoming the ultimate innovator and&nbsp;learning to work with, understand and motivate people as well as create value to sell.</p>
-							
-							<p class="lead">This one is for the geeks and nerds: <i>As a retired coding polyglot, I now focus on coding with Python and JS (React.js to be more specific). The AI dream is getting more real.</i><br></p>
-						</div>
-					</div>
-					
-				</div>
-				
-			</section>
-			
-			<section>
-		    <div class="container">
-		        <div class="row mb64 mb-xs-24">
-		            <div class="col-sm-12 text-center">
-		                <h3>Experience</h3>
-		                <p class="lead">I've been taught to stand out by the quality and impact of my work. If it's not enough I pause, consider and then I work smarter.<br>This is my work experience within the last two years</p>
-		            </div>
-		        </div>
-		        
-		        <div class="row">
-		            <div class="col-md-offset-2 col-md-8">
-		                <div class="tabbed-content button-tabs">
-		                    <ul class="tabs thirds mb64 mb-xs-24">
-		                        <li class="active">
-		                            <div class="tab-title">
-		                                <span>2017</span>
-		                            </div>
-		                            <div class="tab-content text-left">
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/logo - SEO Africa.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">JuNE 2017</h6>
-		                                            <h4>Graduated from Valley View University <br>with a CS Degree. An interest in investing<br>led me to SEO Africa<br></h4>
-		                                        </div>
-		                                    </div>
-		                                    <p>
-												<ol>● CS Major at Valley View University</ol>
-												<ol>● President and Senator, Computing Science Students' Association (2016-2017)</ol>
-												<ol>● Graduated with a 3.31/4.00 CGPA and a 3.87 math CGPA</ol>
-												<ol>● Awarded "Best behaved Student"</ol>
-												<ol>●&nbsp;Admitted into the SEO Africa local internship program to work with Inflexion Capital Limited.</ol>
-												<ol>● Acquired skills during the training program in Accounting &amp; Financial Statement Analysis,&nbsp;Case Study Analysis &amp; Group Presentations,&nbsp;Advanced Microsoft Excel, PowerPoint &amp; Word, Networking,&nbsp;Financial News Review, Fine Dining Etiquette, Networking, Oral and Written Communication</ol>
-											</p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/logo - IC Asset Managers.jpg">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">July 2017 - DECEMBER 2017</h6>
-		                                            <h4><i>Analyst,&nbsp;</i>IC Asset Managers (Ghana) Limited</h4>
-		                                        </div>
-		                                    </div>
-											<p>IC Asset Managers, an Inflexion Capital subsidiary, is a leading fund manager, serving retail and institutional and sovereign clients with over USD 189mn  in assets under management.</p>
-											<p>
-												<ol>● Worked in a team of 8 to increase Assets under Management by 13.5% from USD 167mn to USD 189mn in 6 months</ol>
-												<ol>● Worked with the Chief Operations Officer to optimized the Firm's business processes to guide the development of new software for the firm</ol>
-												<ol>● Wrote test scripts to validate a cash fund software product built for the firm</ol>
-												<ol>● Automated the generation of fund performance reports for all pension funds managed by the firm with MS Power BI</ol>
-												<ol>● Presented on behalf of the fund manager to pension fund clients and trustees</ol>
-												<ol>● Prepared client meeting reports</ol>
-											</p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                            </div>
-		                        </li>
-		                        <li>
-		                            <div class="tab-title">
-		                                <span>2018</span>
-		                            </div>
-		                            <div class="tab-content text-left">
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/logo - Inflexion Capital Limited.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">January 2018 - April 2018</h6>
-		                                            <h4>Analyst, Inflexion Capital Limited</h4>
-		                                        </div>
-		                                    </div>
-		                                    <p>Worked in the technology division: Inflexion Technologies, now a subsidiary: DARE Africa.</p>
-											<p>	
-												<ol>● Developed software requirements and specifications for an instant liquidity investment app
-												<ol>● Built a KPI analytics application to monitor the subsidiary performance with Python and PgSQL. Set up the application on Google Cloud (Compute Engine + Cloud Storage)
-												<ol>● Performed unit and usability tests for software products (cash fund and stock subscription) for IC Asset Managers and IC Securities Holdings Limited<br>
-											</p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/logo - IC Securities.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">MAY 2018 - SEPTEMBER 2018</h6>
-		                                            <h4>Analyst, <br>MTN Ghana - IC Securities (Ghana) Limited</h4>
-		                                        </div>
-		                                    </div>
-											<p>Worked with an Inflexion Subsidiary, IC Securities, to raise USD 787mn for Scancom PLC (MTN Ghana) - the biggest telco in Ghana and most visible brand in the country.</p>
-											<br>IC Securities is an Africa-focused investment banking and securities firm, serving sovereign, institutional, corporate and individual clients.
-											<p>
-												<ol>● Handled technical support for 30 Banks and Brokerage institutions as well as retail investors during the MTN Ghana share offer</ol>
-												<ol>● Performed usability tests on software updates during the share offer</ol>
-												<ol>● Worked on technical and policy documentation for IC Securities Holdings Limited</ol>
-												<ol>● Copywriting for IC Securities TradeLive platform</ol>
-												<ol>● Performed usability tests on IC Securities TradeLive platform</ol>
-												<ol>● Ran financial reconciliations for all USSD subscriptions</ol>
-												<ol>● Devised IC Securities' retail business strategy in a team with management</ol>
-												<ol>● Building a wealth management subsidiary with Inflexion Capital Management</ol>
-											</p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                            </div><div class="tab-content text-left">
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/avatar3.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">January 2019</h6>
-		                                            <h4>???&nbsp;</h4>
-		                                        </div>
-		                                    </div>
-		                                    <p>I haven't started this role yet, but that's where I'm headed.&nbsp;&nbsp;</p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/avatar4.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">???</h6>
-		                                            <h4>MSIT Student, Carnegie Mellon University</h4>
-		                                        </div>
-		                                    </div>
-		                                    <p> </p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                            </div>
-		                        </li>
-		                        <li>
-		                            
-		                            <div class="tab-content text-left">
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/avatar5.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">5:30pm - 06:30pm</h6>
-		                                            <h4>Kane Thompson - Blasting Pesky Bugs</h4>
-		                                        </div>
-		                                    </div>
-		                                    <p>
-		                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-		                                    </p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                                <div>
-		                                    <div class="overflow-hidden">
-		                                        <img alt="Pic" class="mb24 pull-left" src="img/avatar6.png">
-		                                        <div class="pull-left p32 p0-xs pt24">
-		                                            <h6 class="uppercase mb8 number">7:00pm - 08:00pm</h6>
-		                                            <h4>Grace Adler - Tolerating Canned Laughter</h4>
-		                                        </div>
-		                                    </div>
-		                                    <p>
-		                                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.
-		                                    </p>
-		                                    <hr class="mt40 mb40 mt-xs-0 mb-xs-24">
-		                                </div>
-		                            </div>
-		                        </li>
-		                    </ul>
-		                </div>
-		                
-		            </div>
-		        </div>
-		        
-		    </div>
-		    
-		</section></div>
-		
-				
-	<script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/parallax.js"></script>
-        <script src="js/scripts.js"></script>
-        <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-107723907-3', 'auto'); ga('send', 'pageview'); </script>
-</body>
-</html>
-				
+          <p class="subtitle">
+            Also upgrading my website. Check back at the end of January for something new. Probability: 0.5
+          </p>
+
+          <p class="subtitle">
+            View my linkedin profile for more about me: <b><a target="_blank" href="https://linkedin.com/in/csdare">link</a></b>
+          </p>
+
+          <p class="subtitle">
+              Send me an email: <b><a target="_blank" href="mailto:hello@chrisdare.me">hello@chrisdare.me</a> </b>
+          </p>
+      </div>
+    </div>
+  </section>
+  </body>
+    </html>
