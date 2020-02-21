21c081d9a10573054d216007c044583f7843d5f7
Chris Dare <cdare@andrew.cmu.edu>
2020-02-21 13:44:59 +0200

    Update website

--- index.php
+++ index.php
@@ -16,29 +16,41 @@
           </h1>
           
           <p class="subtitle">
-            Entrepreneurial leader and innovator with expertise at the intersection of machine learning, product
-            management, software engineering and communication. Focusses on developing and
-            optimizing solutions for business growth. Past work increased customer bases, revenue
-            and profit margins for clients and partners. Seeking to harnessing applications of AI and
-            machine learning to design, build and manage analytics/cognitive solutions for
-            organizations.
+          I am a computer science grad and former analyst in financial services and gaining ground in artificial intelligence at Carngie Mellon. 
+          Currently building strong expertise at the intersection of communication, data science/machine learning, product management and software engineering. 
+          I intend to use what I learn in this next phase of my life to research, design, build and 
+          manage analytics/cognitive products that solve critical and pressing problems my continent and the rest of the world is facing. 
+          </p>
+
+          <p class="subtitle">This semester at CMU, I am focussing on writing efficient data structures/algorithms, deep learning models as well as processing natural language 
+          and engaging in human-centered design.
           </p>
 
           <p class="subtitle">
-              Currently majoring in applied machine learning at Carnegie Mellon University. 
-              <br><b>Also searching for a summer internship in research/engineering.</b> Areas of interest: Product management, machine learning, deep learning, speech recognition.
+              <br><b>Also searching for a summer internship in research/engineering.</b> Areas of interest:
+              <div>
+                <ol class="subtitle" >
+                  
+                  <li>Research or product development concerning a problem with lots of data</li>
+                  <li>Machine learning and deep learning</li>
+                  <li>Cloud computing/Big data</li>
+                </ol>
+              </div>
+              <br>
+
           </p>
 
           <p class="subtitle">
-            Also upgrading my website. Check back at the end of January for something new. Probability: 0.5
+            Everyone - including me - says I should upgrade my website. But, bayesian statistics tell me that's really bleak at the moment.
           </p>
 
           <p class="subtitle">
+            So.... <br><br>
             View my linkedin profile for more about me: <b><a target="_blank" href="https://linkedin.com/in/csdare">link</a></b>
           </p>
 
           <p class="subtitle">
-              Send me an email: <b><a target="_blank" href="mailto:hello@chrisdare.me">hello@chrisdare.me</a> </b>
+              Also, always happy to chat. Send me an email: <b><a target="_blank" href="mailto:hello@chrisdare.me">hello@chrisdare.me</a> </b>
           </p>
       </div>
     </div>
